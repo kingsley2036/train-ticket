16897af043324e1891a5433f068fe8dd71837b60
kingsley <kingsley2036@qq.com>
2020-10-15 23:34:09 +0800

    初始化redux和拆分视图组件

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,16 +1,27 @@
-import {connect} from "react-redux"
+import { connect } from "react-redux"
+import React from "react"
 import "./App.css"
+import Header from "../common/Header.jsx"
+import DepartDate from "./DepartDate.jsx"
+import HighSpeed from "./HighSpeed.jsx"
+import Journey from "./Journey.jsx"
+import Submit from "./Submit.jsx"
 
 function App() {
- 
+  return (
+    <div>
+      <Header />
+      <Journey />
+      <DepartDate />
+      <HighSpeed />
+      <Submit />
+    </div>
+  )
 }
-function mapStateToProps(state){
-
+function mapStateToProps(state) {
+  return {}
 }
-function mapDispatchToProps(dispatch){
-
+function mapDispatchToProps(dispatch) {
+  return {}
 }
-export default  connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index/store.js
+++ src/index/store.js
@@ -3,6 +3,16 @@ import thunk from 'redux-thunk'
 import reducer from './reducer'
 export default createStore(
     combineReducers(reducer),
-    {},
+    {
+        from: '北京',
+        to: '上海',
+        isCitySelectorVisible: false,
+        currentSelectingLeftCity: false,
+        cityData: null,
+        isLoadingCityData: false,
+        isDateSelectorVisible: false,
+        departDate: Date.now(),
+        highSpeed: false,
+    },
     applyMiddleware(thunk)
 )
