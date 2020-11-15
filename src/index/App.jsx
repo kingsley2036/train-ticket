c07fc450648f998e22c3c24ffc7e7e47c9af8956
kingsley <kingsley2036@qq.com>
2020-11-15 23:25:07 +0800

    日期和城市选择浮层

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,27 +1,113 @@
 import { connect } from "react-redux"
-import React from "react"
-import "./App.css"
+import { bindActionCreators } from "redux"
+import React, { useCallback, useMemo } from "react"
 import Header from "../common/Header.jsx"
 import DepartDate from "./DepartDate.jsx"
 import HighSpeed from "./HighSpeed.jsx"
 import Journey from "./Journey.jsx"
 import Submit from "./Submit.jsx"
+import CitySelector from "../common/CitySelector.jsx"
+import DateSelector from "../common/DateSelector.jsx"
+import {
+  exchangeFromTo,
+  showCitySelector,
+  hideCitySelector,
+  fetchCityData,
+  setSelectedCity,
+  showDateSelector,
+  hideDateSelector,
+  setDepartDate
+} from "./actions"
+import "./App.css"
+import { h0 } from '../common/fp';
+function App(props) {
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
+  const {
+    from,
+    to,
+    dispatch,
+    isCitySelectorVisible,
+    cityData,
+    isLoadingCityData,
+    departDate,
+    isDateSelectorVisible
+  } = props
+  const cbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        exchangeFromTo,
+        showCitySelector,
+      },
+      dispatch
+    )
+  }, [dispatch])
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators(
+      {
+        onBack: hideCitySelector,
+        fetchCityData,
+        onSelect: setSelectedCity,
+      },
+      dispatch
+    )
+  }, [dispatch])
+  const departDateCbs = useMemo(() => {
+    return bindActionCreators(
+        {
+            onClick: showDateSelector,
+        },
+        dispatch
+    );
+}, [dispatch]);
+const DateSelectorCbs= useMemo(() => {
+  return bindActionCreators(
+      {
+          onBack: hideDateSelector
+      },
+      dispatch
+  );
+}, [dispatch]);
+const onSelectDate=useCallback(
+  (day) => {
+    if(!day){
+      return
+    }
+    if(day<h0){
+      return
+    }
+    dispatch(setDepartDate(day))
+    dispatch(hideDateSelector())
+  },
+  [dispatch],
+)
 
-function App() {
   return (
     <div>
-      <Header />
-      <Journey />
-      <DepartDate />
-      <HighSpeed />
-      <Submit />
+      <div className="header-wrapper">
+        <Header title="火车票" onBack={onBack} />
+      </div>
+      <form className="form">
+        <Journey from={from} to={to} {...cbs} />
+        <DepartDate {...departDateCbs} time={departDate} />
+        <HighSpeed />
+        <Submit />
+      </form>
+      <CitySelector
+        show={isCitySelectorVisible}
+        cityData={cityData}
+        isLoading={isLoadingCityData}
+        {...citySelectorCbs}
+      />
+      <DateSelector show={isDateSelectorVisible} {...DateSelectorCbs}  onSelect={onSelectDate}/>
     </div>
   )
 }
 function mapStateToProps(state) {
-  return {}
+  return state
 }
 function mapDispatchToProps(dispatch) {
-  return {}
+  return { dispatch }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App)
